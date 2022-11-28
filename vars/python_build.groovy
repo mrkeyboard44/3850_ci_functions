0947937acf90223dccda2c5cb2a79ea74493dba4
mrkeyboard <matthewdandar@gmail.com>
2022-11-27 20:57:49 -0800

    added remove container script

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -42,6 +42,7 @@ def call(dockerRepoName, serviceName, portNum) {
 				}
 				steps {
 					dir("3850_assignment") {
+						sh "chmod +x remove_container.sh"
 						sh "docker-compose down || true && ./remove_container.sh '${serviceName}' || true"
 						sh "docker-compose up -d"
 					}
